648faabdb7c2e19d9888d8bdbd392793af41cab2
chris (daiyi) <daiyitastic@gmail.com>
2017-06-19 23:48:02 +0200

    list priorities

--- readme.md
+++ readme.md
@@ -11,3 +11,13 @@ see application state:
 ```
 re-frame.db/app-db
 ```
+
+
+#### next up
+* request geolocation (lat, lng) from browser
+* first pass on cute ui
+* offline!
+* second pass on cute ui
+  * colours
+  * stars n sun
+* more views: graphs n charts
--- src/cljs/sonnenzeit/db.cljs
+++ src/cljs/sonnenzeit/db.cljs
@@ -2,7 +2,7 @@
   (:require [re-frame.core :as re-frame]))
 
 (def default-db
-  {:name "\\o/"
+  {:name "sonnenzeit"
    :time (js/Date.)
-   :sunset-time "2017-06-17T00:00:00+00:00"
+   :sunset-time "2017-06-17T00:00:00+00:00" ;; todo better default
    :ajax-status nil})
