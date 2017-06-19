c21715760c26314f241b672fa5de1a214ad95379
chris (daiyi) <daiyitastic@gmail.com>
2017-06-20 01:52:10 +0200

    gh-pages

--- src/cljs/sonnenzeit/config.cljs
+++
@@ -1,4 +0,0 @@
-(ns sonnenzeit.config)
-
-(def debug?
-  ^boolean goog.DEBUG)
--- src/cljs/sonnenzeit/db.cljs
+++
@@ -1,8 +0,0 @@
-(ns sonnenzeit.db
-  (:require [re-frame.core :as re-frame]))
-
-(def default-db
-  {:name "sonnenzeit"
-   :time (js/Date.)
-   :sunset-time "2017-06-17T00:00:00+00:00" ;; todo better default
-   :status nil})
